9b403f720b73c581d5655f70b123dbe201393918
jaehue <jaehue@jang.io>
2015-03-25 08:59:53 +0900

    add new type webSocketConn - fullfill Messager interface

--- web_socket.go
+++ web_socket.go
@@ -4,9 +4,12 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"time"
 )
 
 const socketBufferSize = 1024
+
+type webSocketConn websocket.Conn
 
 var upgrader = &websocket.Upgrader{
 	ReadBufferSize:  socketBufferSize,
@@ -20,5 +23,32 @@ func handleWebsocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println("connect socket. addr: ", socket.RemoteAddr())
-	newClient(socket)
+	var messager webSocketConn = webSocketConn(*socket)
+	newClient(&messager)
+}
+
+func (c *webSocketConn) ReadChatMessage(msg *Message) error {
+	var conn websocket.Conn = websocket.Conn(*c)
+
+	if err := conn.ReadJSON(&msg); err != nil {
+		return err
+	}
+	msg.CreatedAt = time.Now()
+	log.Println("read from websocket:", msg)
+	return nil
+}
+
+func (c *webSocketConn) WriteChatMessage(msg *Message) error {
+	var conn websocket.Conn = websocket.Conn(*c)
+	return conn.WriteJSON(msg)
+}
+
+func (c *webSocketConn) Close() error {
+	var conn websocket.Conn = websocket.Conn(*c)
+	return conn.Close()
+}
+
+func (c *webSocketConn) String() string {
+	var conn websocket.Conn = websocket.Conn(*c)
+	return conn.RemoteAddr().String()
 }
